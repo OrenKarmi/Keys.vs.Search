f4bba16d2f75c5cc2b3ce1cf62395685736ca47b
Oren Karmi <48683990+OrenKarmi@users.noreply.github.com>
2023-09-21 16:57:05 +0300

    Update keys-search-comparison.py
    support json and hash

--- keys-search-comparison.py
+++ keys-search-comparison.py
@@ -7,85 +7,53 @@ from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.query import NumericFilter, Query
 import json
 import time
+from load_keys import connect
+from load_keys import create_index
 
-
-# Connect
-def connect(host,port):
-    ## Connect
-    r = redis.Redis(host=f"{host}", port=f"{port}", decode_responses=True)
-    return r
-
-
-def create_index(r):
-    ## Define search schema
-    schema = (
-        TextField("$.name", as_name="name"),
-        TagField("$.city", as_name="city"),
-        NumericField("$.age", as_name="age")
-    )
-     
-    ## Create an index
-    rs = r.ft("idx:users")
-    idx_name = "idx_name_init"
-    json_string = "json_str_init"
-
-
-    try:
-        ## Check if the index exists
-        idx_info = rs.info()
-        idx_name = "idx:users"
-    
-        # Convert the JSON object to a string
-        json_string = json.dumps(idx_info)
-    
-    except Exception as e:
-        # The index already exist
-        a=1
-
-    if idx_name in json_string:
-        a=1
-        # index already exists
-    else:
-        # creating idx:users
-        rs.create_index(
-            schema,
-            definition=IndexDefinition(
-                prefix=["user:"], index_type=IndexType.JSON
-            )
-        )
-    return rs
-    
 
 def main():
     # Connect to Redis
-    r1 = connect('redis-12087.karmi1-cluster.primary.cs.redislabs.com', 12087)
+    r1 = connect('redis-10645.karmi1-cluster.primary.cs.redislabs.com', 10645)
 
-    # Create an index for users
-    rs = create_index(r1)
+    # Create JSON and HASH indexes for users
+    rs_json = create_index(r1, "idx:json_users", IndexType.JSON)
+    rs_hash = create_index(r1, "idx:hash_users", IndexType.HASH)
 
+    # Perform the comparison
+    # *** KEYS ***
     # measure the time elapsed for the scan/ keys query
     start_time = time.time()
-    res = r1.keys(pattern='user:40000*')
+    res = r1.keys(pattern='user:hash:40000*')
     end_time = time.time()
     elapsed_time = end_time - start_time
     print(f"KEYS Result is: ",res)
-    print(f"Elapsed time: {elapsed_time:.4f} seconds")
+    print(f"*** KEYS Elapsed time: {elapsed_time:.4f} seconds")
 
+    # *** JSON ***
     # measure the time elapsed for the search query
     start_time = time.time()
     # Search name and age range
-    res = rs.search(
+    res = rs_json.search(
             Query("Paul @age:[400001 400009]")
             )
     end_time = time.time()
     elapsed_time = end_time - start_time
-    print(f"FT.SEARCH Result is: ",res)
-    print(f"Elapsed time: {elapsed_time:.4f} seconds")
+    print(f"FT.SEARCH for JSON Result is: ",res)
+    print(f"*** JSON Search Elapsed time: {elapsed_time:.4f} seconds")
+
+    # *** HASH ***
+    # measure the time elapsed for the search query
+    start_time = time.time()
+    # Search name and age range
+    res = rs_hash.search(
+            Query("Paul @age:[400001 400009]")
+            )
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"FT.SEARCH for HASH Result is: ",res)
+    print(f"*** HASH Search Elapsed time: {elapsed_time:.4f} seconds")
 
 
 if __name__ == "__main__":
     # Call the main function when the script is executed
     main()
-
-
-
